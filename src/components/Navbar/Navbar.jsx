91b41b593818338ecbb80c372eda2991ee5f9f35
Illaya Bharathi <illayabharathi1112@gmail.com>
2025-07-21 16:18:54 +0530

    updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,46 +5,56 @@ import { CiSearch } from "react-icons/ci";
 import { FaBell } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
 import smily from "../../assets/smily.jpg"
-import {logout} from '../../firebase'
+import { logout } from '../../firebase'
 
 const Navbar = () => {
-
   const navRef = useRef()
-  useEffect(()=>{
-      window.addEventListener('scroll',()=>{
-        if(window.scrollY >= 80){
-          navRef.current.classList.add('nav-dark')
-        }else{
-           navRef.current.classList.remove('nav-dark')
-        }
-      })
-  },[])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!navRef.current) return;
+      if (window.scrollY >= 80) {
+        navRef.current.classList.add('nav-dark')
+      } else {
+        navRef.current.classList.remove('nav-dark')
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
     <div ref={navRef} className='navbar'>
-        <div className="navbar-left">
-            <img src={logo} alt="" />
-            <ul>
-                <li>Home</li>
-                <li>TV Shows</li>
-                <li> Movies</li>
-                <li>New & Popular</li>
-                <li>My List</li>
-                <li>Browse by Language</li>
-            </ul>
-        </div>
-        <div className="navbar-right"> 
-           <CiSearch  className='icons'/>
-             <p>Children</p>
-                <FaBell className='icons'/>
-             <div className='navbar-profile'>
-           <CgProfile className='profile' />
-           <img src={smily} alt="" className='smily'/>
-             <div className="dropdown">
-                <p onClick={()=>{logout()}}>Sign out of netflix</p>
-             </div>
-                </div>       
-        </div>
+      {/* Left side - Logo and Menu */}
+      <div className="navbar-left">
+        <img src={logo} alt="Netflix Logo" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Language</li>
+        </ul>
+      </div>
 
+      {/* Right side - Icons, profile, and dropdown */}
+      <div className="navbar-right">
+        <CiSearch className='icons' />
+        <p>Children</p>
+        <FaBell className='icons' />
+        <div className='navbar-profile'>
+          <CgProfile className='profile' />
+          <img src={smily} alt="Profile Smily" className='smily' />
+          <div className="dropdown">
+            <p onClick={() => logout()}>Sign out of Netflix</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
